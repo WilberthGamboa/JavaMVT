5c02202aedbf250ced0ad8d81bd141a5522efd73
Your Name~ <wgcampos15@gmail.com>
2022-05-16 14:36:49 -0500

    first commit

---
+++ src/main/java/model/soswing/SOSWING.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package model.soswing;
+
+/**
+ *
+ * @author Wilberth
+ */
+public class SOSWING {
+
+    public static void main(String[] args) {
+        Plantilla plantilla = new Plantilla();
+        plantilla.setVisible(true);
+     
+    }
+}
